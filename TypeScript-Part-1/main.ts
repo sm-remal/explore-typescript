21f22a64339528eb1582dc3e60b26fce53ff3356
sm-remal <remal.mozzem@gmail.com>
2026-02-13 11:28:11 +0600

    type practice

---
+++ TypeScript-Part-1/main.ts
@@ -0,0 +1,9 @@
+let firstName = "Shah Mozzem Remal";
+let age = 23;
+let skill = "MERN Stack Developer";
+let isSenior = false;
+
+firstName = 90;   // Wrong
+firstName = "Remal"  // Right
+
+console.log(firstName, age, skill, isSenior)
--- main.ts
+++
@@ -1,5 +0,0 @@
-const username: string = "Shah Mozzem Remal"
-const age: number = 25
-const isDeveloper: boolean = true
-
-console.log(username, age, isDeveloper)
