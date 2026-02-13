44a6fd85d08e55d7852e66ae7f5e981f8e7c5c3e
sm-remal <remal.mozzem@gmail.com>
2026-02-13 11:32:55 +0600

    define type

--- TypeScript-Part-1/main.ts
+++ TypeScript-Part-1/main.ts
@@ -3,7 +3,21 @@ let age = 23;
 let skill = "MERN Stack Developer";
 let isSenior = false;
 
-firstName = 90;   // Wrong
-firstName = "Remal"  // Right
+// firstName = 90;   // Wrong
+// firstName = "Remal"  // Right
 
 console.log(firstName, age, skill, isSenior)
+
+
+// ========================================== //
+
+let price: number = 100;
+let color: string = "White";
+let isNew: boolean = true;
+
+console.log(price, color, isNew);
+
+
+// ======================================== //
+
+
