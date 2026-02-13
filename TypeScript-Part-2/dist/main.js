2d86f243a4cc70a65e5253c74f658123784c30e4
sm-remal <remal.mozzem@gmail.com>
2026-02-13 12:32:20 +0600

    type function

---
+++ TypeScript-Part-2/dist/main.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const price = 100;
+console.log(price);
+//# sourceMappingURL=main.js.map
---
+++ TypeScript-Part-2/src/main.ts
@@ -0,0 +1,5 @@
+const ageCal = (name: string, age: number): string => {
+    return `I am ${name}. And I am ${age} years old`
+}
+
+ageCal("Shah Mozzem Remal", 23)
