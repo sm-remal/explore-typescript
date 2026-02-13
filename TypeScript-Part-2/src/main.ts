365cc1f81e90a9d4a7e2a424372df0a04a0f9d24
sm-remal <remal.mozzem@gmail.com>
2026-02-13 15:21:35 +0600

    function

--- TypeScript-Part-2/src/main.ts
+++ TypeScript-Part-2/src/main.ts
@@ -2,4 +2,31 @@ const ageCal = (name: string, age: number): string => {
     return `I am ${name}. And I am ${age} years old`
 }
 
-ageCal("Shah Mozzem Remal", 23)
+ageCal("Shah Mozzem Remal", 23);
+
+
+// ====================================================== // 
+
+const myFunction = (name: string = "Bangladesh", amount: number = 140000): string => {
+    return `The name of our country is ${name} about ${amount} people live here.`
+}
+
+myFunction();
+
+
+// ===================================================== //
+
+const yourFunction = (name: string = "Bangladesh", amount: number = 140000): void => {
+    console.log(`The name of our country is ${name} about ${amount} people live here.`);
+}
+
+yourFunction();
+
+
+// =================================================== //
+
+const yourFunction2 = (name: string = "Bangladesh", amount: number = 140000): never => {
+    throw new Error();
+}
+
+yourFunction2();
