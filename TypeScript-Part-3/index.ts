484f956939f29c3789a03b144ce842e07bf1f019
sm-remal <remal.mozzem@gmail.com>
2026-02-13 15:58:11 +0600

    array

---
+++ TypeScript-Part-3/index.ts
@@ -0,0 +1,20 @@
+const fruits: string[] = ["Mango", "Apple", "Banana", "Orange"];
+
+fruits.push("Watermelon")
+
+// ================================= //
+
+const food: (string | number)[] = ["Kacchi", "Polao", "Gorur-Mangsho", 60];
+
+
+console.log(fruits, food)
+
+
+
+// =============================== //
+
+// tuples 
+const dev: [string, number, boolean] = ["Remal", 23, false];
+dev[0] = "Siam";
+dev[1] = 24;
+console.log(dev);
