f68e86a50a98fa1394b8ebaaa4c9ee5e7ae858af
sm-remal <remal.mozzem@gmail.com>
2026-02-14 23:03:40 +0600

    practice problem

---
+++ Pracatice/main.ts
@@ -0,0 +1,22 @@
+// problem - 1
+const fullName: string = "Shah Mozzem Remal";
+const age: number = 20;
+const isDeveloper: boolean = true;
+
+console.log(fullName, age, isDeveloper)
+
+// problem - 2
+function add(a: number, b: number): number {
+    return a + b;
+}
+const result = add(3, 6)
+console.log(result)
+
+// problem - 3
+const array: number[] = [1, 2, 3, 4, 5]
+let sum: number = 0;
+for(const main of array){
+    sum = sum + main;
+}
+console.log(sum)
+
