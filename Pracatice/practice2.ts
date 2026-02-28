c1a1f4aaf7e532ecd83019fee456764b4d2c9c12
sm-remal <remal.mozzem@gmail.com>
2026-02-28 21:04:34 +0600

    practice2

---
+++ Pracatice/practice2.ts
@@ -0,0 +1,14 @@
+// problem 1
+function add(a: number, b: number): number {
+  return a + b;
+}
+
+// console.log(add(5,6));
+
+//problem 2
+function printId(id: number | string){
+  console.log(id);
+}
+printId("512");
+
+
