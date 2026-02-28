05f3e6a259a829cef0cb98a6738c00b0857ed10b
sm-remal <remal.mozzem@gmail.com>
2026-02-28 21:47:30 +0600

    problem 3, 4, 5

--- Pracatice/practice2.ts
+++ Pracatice/practice2.ts
@@ -6,9 +6,69 @@ function add(a: number, b: number): number {
 // console.log(add(5,6));
 
 //problem 2
-function printId(id: number | string){
+function printId(id: number | string): void{
   console.log(id);
 }
-printId("512");
+// printId("512");
+
+// problem 3
+interface Book {
+    id: number,
+    title: string,
+    author: string,
+    price: number,
+    isStock: boolean,
+}
+
+const book: Book = {
+    id: 125,
+    title: "Harry Pother",
+    author: "Some-One",
+    price: 525,
+    isStock: true,
+}
+
+// console.log(book.title)
 
 
+// problem 4
+interface User {
+    id: number,
+    name: string,
+    email: string,
+    phone: string,
+}
+
+const user: User = {
+    id: 101,
+    name: "Asif Hasan",
+    email: "asifhasan@gmail.com",
+    phone: "01234567894"
+}
+
+console.log(user.phone)
+
+
+// problem 5
+interface Book {
+    id: number,
+    title: string,
+    author: string,
+    price: number,
+    isStock: boolean,
+}
+
+const book1: Book = {
+    id: 321,
+    title: "The Investor",
+    author: "Someone",
+    price: 550,
+    isStock: true,
+
+}
+
+function getBookInfo(book: Book): string {
+   return `${book.title} by ${book.author}`
+}
+
+console.log(getBookInfo(book1))
